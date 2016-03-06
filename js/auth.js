0d227286cace0524e01d2d41c4f379c6cdc13b39
Benson Li <bl4074@hotmail.com>
2016-03-06 15:50:55 -0800

    Testing code reach

--- js/auth.js
+++ js/auth.js
@@ -39,6 +39,7 @@ function handleAuthResult(authResult) {
     // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
     // client flow. The current function is called when that flow completes.
     $('#login-link').click(function() {
+      alert();
       gapi.auth.authorize({
         client_id: OAUTH2_CLIENT_ID,
         scope: OAUTH2_SCOPES,
